5f5f5cfbd306db05afdf8bd9fa858b99a513c577
Emmanuel <emrade95@gmail.com>
2019-08-26 15:05:53 +0000

    Modified how it works page

--- resources/views/how_it_works.blade.php
+++ resources/views/how_it_works.blade.php
@@ -26,7 +26,10 @@
                     <h1 class="tab__title" data-tab="header">Looking to hire</h1>
                     <div class="tab__body" data-tab="body">
                         <div class="row align-items-center">
-                            <div class="col-md-6 col-sm-12">
+                            <div class="col-md-6 col-sm-12 order-lg-2 py-5">
+                                <img class="img-center" src="https://www.upwork.com/ab/assets/Adquiro/5db88cd/wp/images/find-CL.eac27d3a9e54.svg">
+                            </div>
+                            <div class="col-md-6 col-sm-12 order-lg-1">
                                 <h1 class="section-head">Easily find quality matches</h1>
                                 <p class="pt-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
                                     Mauris non gravida arcu. Nam scelerisque nunc commodo sem accumsan, vitae scelerisque urna facilisis. 
@@ -35,14 +38,11 @@
                                     Phasellus varius libero sed commodo porta. Aliquam et lacus tincidunt, 
                                     tincidunt ipsum eget, ornare augue. Morbi blandit fermentum nibh, non aliquam est vestibulum eu. 
                                 </p>
-                            </div>
-                            <div class="col-md-6 col-sm-12">
-                                <img class="img-center" src="https://www.upwork.com/ab/assets/Adquiro/5db88cd/wp/images/find-CL.eac27d3a9e54.svg">
-                            </div>
+                            </div>                        
                         </div>
 
                         <div class="row pt-4 align-items-center">
-                            <div class="col-md-6 col-sm-12">
+                            <div class="col-md-6 col-sm-12 py-5">
                                 <img class="img-center" src="https://www.upwork.com/ab/assets/Adquiro/5db88cd/wp/images/hire.9f5208d54080.svg">
                             </div>
                             <div class="col-md-6 col-sm-12">
@@ -58,7 +58,11 @@
                         </div>
 
                         <div class="row pt-4 align-items-center">
-                            <div class="col-md-6 col-sm-12">
+                            <div class="col-md-4 col-sm-12 order-lg-3 py-5">
+                                <img class="img-center" src="https://www.upwork.com/ab/assets/Adquiro/5db88cd/wp/images/work.4f748c9707ba.svg" height="400px">
+                            </div>                            
+                            <div class="col-md-2 order-lg-2"></div>
+                            <div class="col-md-6 col-sm-12 order-lg-1">
                                 <h1 class="section-head">Work efficiently, effectively</h1>
                                 <p class="pt-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
                                     Mauris non gravida arcu. Nam scelerisque nunc commodo sem accumsan, vitae scelerisque urna facilisis. 
@@ -68,14 +72,11 @@
                                     tincidunt ipsum eget, ornare augue. Morbi blandit fermentum nibh, non aliquam est vestibulum eu. 
                                 </p>
                             </div>
-                            <div class="col-md-2"></div>
-                            <div class="col-md-4 col-sm-12">
-                                <img class="img-center" src="https://www.upwork.com/ab/assets/Adquiro/5db88cd/wp/images/work.4f748c9707ba.svg" height="400px">
-                            </div>
+                            
                         </div>
 
                         <div class="row pt-4 align-items-center">
-                            <div class="col-md-6 col-sm-12">
+                            <div class="col-md-6 col-sm-12 py-5">
                                 <img src="https://www.upwork.com/ab/assets/Adquiro/5db88cd/wp/images/payment.e26f09fb5308.svg">
                             </div>
                             <div class="col-md-6 col-sm-12">
@@ -96,7 +97,71 @@
                   <article class="tab__content" id="tab-two">
                     <h1 class="tab__title" data-tab="header">Wanting to be hired</h1>
                     <div class="tab__body" data-tab="body">
-                        <p>YES, Wanting to be hired.</p>
+                        <div class="row align-items-center">
+                            <div class="col-md-6 col-sm-12 order-lg-2 py-5">
+                                <img class="img-center" src="https://www.upwork.com/ab/assets/Adquiro/5db88cd/wp/images/find-CL.eac27d3a9e54.svg">
+                            </div>
+                            <div class="col-md-6 col-sm-12 order-lg-1">
+                                <h1 class="section-head">Find rewarding projects</h1>
+                                <p class="pt-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                                    Mauris non gravida arcu. Nam scelerisque nunc commodo sem accumsan, vitae scelerisque urna facilisis. 
+                                    Fusce non sagittis libero, bibendum feugiat mi. Donec eget augue nec lorem bibendum bibendum tincidunt a lorem. 
+                                    Vivamus lacinia neque odio, in dapibus nulla rhoncus eu. Duis ut felis nec libero consectetur dictum eu eget est. 
+                                    Phasellus varius libero sed commodo porta. Aliquam et lacus tincidunt, 
+                                    tincidunt ipsum eget, ornare augue. Morbi blandit fermentum nibh, non aliquam est vestibulum eu. 
+                                </p>
+                            </div>                        
+                        </div>
+
+                        <div class="row pt-4 align-items-center">
+                            <div class="col-md-6 col-sm-12 py-5">
+                                <img class="img-center" src="https://www.upwork.com/ab/assets/Adquiro/5db88cd/wp/images/hire.9f5208d54080.svg">
+                            </div>
+                            <div class="col-md-6 col-sm-12">
+                                <h1 class="section-head">Get hired quickly</h1>
+                                <p class="pt-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                                    Mauris non gravida arcu. Nam scelerisque nunc commodo sem accumsan, vitae scelerisque urna facilisis. 
+                                    Fusce non sagittis libero, bibendum feugiat mi. Donec eget augue nec lorem bibendum bibendum tincidunt a lorem. 
+                                    Vivamus lacinia neque odio, in dapibus nulla rhoncus eu. Duis ut felis nec libero consectetur dictum eu eget est. 
+                                    Phasellus varius libero sed commodo porta. Aliquam et lacus tincidunt, 
+                                    tincidunt ipsum eget, ornare augue. Morbi blandit fermentum nibh, non aliquam est vestibulum eu. 
+                                </p>
+                            </div>
+                        </div>
+
+                        <div class="row pt-4 align-items-center">
+                            <div class="col-md-4 col-sm-12 order-lg-3 py-5">
+                                <img class="img-center" src="https://www.upwork.com/ab/assets/Adquiro/5db88cd/wp/images/work.4f748c9707ba.svg" height="400px">
+                            </div>                            
+                            <div class="col-md-2 order-lg-2"></div>
+                            <div class="col-md-6 col-sm-12 order-lg-1">
+                                <h1 class="section-head">Work efficiently, effectively</h1>
+                                <p class="pt-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                                    Mauris non gravida arcu. Nam scelerisque nunc commodo sem accumsan, vitae scelerisque urna facilisis. 
+                                    Fusce non sagittis libero, bibendum feugiat mi. Donec eget augue nec lorem bibendum bibendum tincidunt a lorem. 
+                                    Vivamus lacinia neque odio, in dapibus nulla rhoncus eu. Duis ut felis nec libero consectetur dictum eu eget est. 
+                                    Phasellus varius libero sed commodo porta. Aliquam et lacus tincidunt, 
+                                    tincidunt ipsum eget, ornare augue. Morbi blandit fermentum nibh, non aliquam est vestibulum eu. 
+                                </p>
+                            </div>
+                            
+                        </div>
+
+                        <div class="row pt-4 align-items-center">
+                            <div class="col-md-6 col-sm-12 py-5">
+                                <img src="https://www.upwork.com/ab/assets/Adquiro/5db88cd/wp/images/payment.e26f09fb5308.svg">
+                            </div>
+                            <div class="col-md-6 col-sm-12">
+                                <h1 class="section-head">Get paid on time</h1>
+                                <p class="pt-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                                    Mauris non gravida arcu. Nam scelerisque nunc commodo sem accumsan, vitae scelerisque urna facilisis. 
+                                    Fusce non sagittis libero, bibendum feugiat mi. Donec eget augue nec lorem bibendum bibendum tincidunt a lorem. 
+                                    Vivamus lacinia neque odio, in dapibus nulla rhoncus eu. Duis ut felis nec libero consectetur dictum eu eget est. 
+                                    Phasellus varius libero sed commodo porta. Aliquam et lacus tincidunt, 
+                                    tincidunt ipsum eget, ornare augue. Morbi blandit fermentum nibh, non aliquam est vestibulum eu. 
+                                </p>
+                            </div>
+                        </div>
                     </div>
                   </article>
                 </section>
