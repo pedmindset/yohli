dd3fae5d0af75464695cdcadf536f3859d79dd4d
Emmanuel <emrade95@gmail.com>
2019-09-24 19:21:47 +0000

    Release Payment Done

--- resources/views/dashboard/milestones.blade.php
+++ resources/views/dashboard/milestones.blade.php
@@ -17,7 +17,14 @@ Milestones for <a href="#">Food Delivery Mobile Application</a>
             <div class="content pb-1">
                 <ul class="timeline">
                     <li class="event done">
-                        <h3>UI Design <i class="icon-material-outline-check-circle text-success"></i></h3>
+                        <h3>UI Design 
+                            <i class="icon-material-outline-check-circle text-success"></i>
+                            <span class="float-right">
+                                <a href="#small-dialog-1"  class="popup-with-zoom-anim button btn-xs">
+                                    Release Payment
+                                </a>
+                            </span>
+                        </h3>
                         <p>$600</p>
                     </li>
                     <li class="event notdone">
@@ -30,4 +37,39 @@ Milestones for <a href="#">Food Delivery Mobile Application</a>
         </div>
     </div>
 </div>
+
+<!-- Bid Acceptance Popup
+================================================== -->
+<div id="small-dialog-1" class="zoom-anim-dialog mfp-hide dialog-with-tabs">
+
+	<!--Tabs -->
+	<div class="sign-in-form">
+
+		<ul class="popup-tabs-nav">
+			<li><a href="#tab1">Release Payment</a></li>
+		</ul>
+
+		<div class="popup-tabs-container">
+
+			<!-- Tab -->
+			<div class="popup-tab-content" id="tab">
+				
+				<!-- Welcome Text -->
+				<div class="welcome-text">
+					<h3>Release Payment to David for Milestone?</h3>
+					<div class="bid-acceptance margin-top-15">
+						$600
+					</div>
+
+				</div>
+
+				<!-- Button -->
+				<button class="margin-top-15 button full-width button-sliding-icon ripple-effect" type="submit" form="terms">Yes, Release <i class="icon-material-outline-check-circle"></i></button>
+
+			</div>
+
+		</div>
+	</div>
+</div>
+<!-- Bid Acceptance Popup / End -->
 @endsection
